4febea9f96816ecd3d77230f17edd99e012e656a
bentaylor <ben10taylor10@hotmail.com>
2021-03-01 15:47:48 +0000

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f7858cc3520c286ca09a8eed4cf0053d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6bd6ce14-8316-4812-a895-f4f9f6da2a82'
+};
